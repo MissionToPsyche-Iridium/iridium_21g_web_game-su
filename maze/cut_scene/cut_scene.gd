b3230f00c9be848e03b5e8b379e0555a3a47c66b
anysketch <glane0900@gmail.com>
2025-01-15 15:15:19 -0800

    Update Maze level cut scene and end scene

--- maze/cut_scene/cut_scene.gd
+++ maze/cut_scene/cut_scene.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
-func _on_button_2_pressed() -> void:
-		get_tree().change_scene_to_file("res://maze/maze.tscn")
+func _on_continue_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://maze/maze.tscn")
--- maze/end_scene/end_scene.gd
+++ maze/end_scene/end_scene.gd
@@ -5,3 +5,7 @@ extends Control
 
 
 	
+
+
+#func _on_continue_pressed() -> void:
+	#get_tree().change_scene_to_file("res://")
