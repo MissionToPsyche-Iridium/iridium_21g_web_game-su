0718ca21c97260b6a9c563e037a55e6f2f788978
anysketch <glane0900@gmail.com>
2025-02-08 11:35:49 -0800

    Updated start menu with new transition

--- start_menu/start_menu.gd
+++ start_menu/start_menu.gd
@@ -1,5 +1,17 @@
 extends Control
 
 
-func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://maze/cut_scene/cut_scene.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Button.hide()
+	$AnimationPlayer.play("cover_fade")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	await get_tree().create_timer(8.2).timeout
+	$Button.show()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scavenger-hunt/room1/room1.tscn")
