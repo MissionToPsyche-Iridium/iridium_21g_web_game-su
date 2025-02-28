1a7da26789248d087fe0da13b88e5df1d0bf210b
anysketch <glane0900@gmail.com>
2025-02-28 13:16:27 -0800

    Add continue button to the disclaimer message

--- start_menu/start_menu.gd
+++ start_menu/start_menu.gd
@@ -6,40 +6,45 @@ func _ready() -> void:
 	$Cover.show()
 	$ButtonCover.show()
 	$Disclaimer.hide()
-	$Icon.show()
-	#$AnimationPlayer.play("cover_fade")
-	await get_tree().create_timer(2.0).timeout      # wait 8 seconds to let cover cover disclaimer
-	$AnimationPlayer.play("cover")
-	await get_tree().create_timer(1.0).timeout 
-	$Icon.hide()
-	$Disclaimer.show()
-	await get_tree().create_timer(1.0).timeout 
-	$AnimationPlayer.play("cover_fade")
-	await get_tree().create_timer(8.2).timeout      # wait 8 seconds to let cover cover disclaimer
-	$AnimationPlayer.play("cover")
-	await get_tree().create_timer(1.2).timeout
+	$Continue.hide()
+	$ContinueCover.color = Color(0, 0, 0, 255)
+	$ContinueCover.show()
+	$Logo.show()
+	await get_tree().create_timer(2.0).timeout      # Wait 2 seconds before covering the Psyche logo
+	$AnimationPlayer.play("cover")                  # Cover Psyche logo - "fade to black"
+	await get_tree().create_timer(1.0).timeout      # Wait 1 second to let the animation to finish
+	$Logo.hide()                                    # Hide the Pysche logo
+	$Disclaimer.show()                              # Show the disclaimer 
+	$Continue.show()                                # Show continue button 
+	await get_tree().create_timer(1.0).timeout      # Wait 1 second
+	$AnimationPlayer.play("cover_fade")             # Fade the cover to display the disclaimer 
+	await get_tree().create_timer(10.0).timeout     # Wait 10 seconds
+	$Cover.hide()                                   # Hide the cover to allow buttons to be pressed 
+	$AnimationPlayer.play("continue_fade")          # Fade away the cover over the continue button
+	await get_tree().create_timer(1.0).timeout      # Wait 1 second to let the animation to finish 
+	$ContinueCover.hide()                           # Hide the cover to allow button to be pressed 
+	
+func _on_continue_pressed() -> void:                # When the continue button pressed
+	$Cover.show()                                   # Show the cover
+	$AnimationPlayer.play("cover")                  # Cover the disclaimer - "fade to black"
+	await get_tree().create_timer(1.0).timeout      # Wait 1 second to let the animation to finish
 	$Disclaimer.hide()
-	$ButtonCover.hide()
-	$AnimationPlayer.play("cover_fade")
-	await get_tree().create_timer(1.0).timeout
-	$Cover.hide()
+	$Continue.hide()                                # Hide the continue button
+	$ButtonCover.hide()                             # Hide the button cover to show the start and credits buttons
+	$AnimationPlayer.play("cover_fade")             # Fade the cover 
+	await get_tree().create_timer(1.0).timeout      # Wait 1 minute to let the animation finish 
+	$Cover.hide()                                   # Hide the cover to allow buttons to be pressed 
 
 
-func _on_start_pressed() -> void:
-	$Cover.show()
-	$AnimationPlayer.play("cover")
-	await get_tree().create_timer(1.2).timeout
-	$Cover.color = Color(0,0,0,255)
-	get_tree().change_scene_to_file("res://scavenger-hunt/world.tscn")
+func _on_start_pressed() -> void:                   # When the start button is pressed 
+	$Cover.show()                                   # Show the cover 
+	$AnimationPlayer.play("cover")                  # Cover the start menu - "fade to black"
+	await get_tree().create_timer(1.0).timeout      # Wait 1 second to let the animation finish 
+	get_tree().change_scene_to_file("res://scavenger-hunt/world.tscn")   # Change the scence to the scavenger hunt minigame 
 
 
-func _on_credits_pressed() -> void:
-	$Cover.show()
-	$AnimationPlayer.play("cover")
-	await get_tree().create_timer(1.1).timeout
-	$Cover.color = Color(0,0,0,255)
-	get_tree().change_scene_to_file("res://Credits scene/credits.tscn")
-
-
-func _on_feedback_pressed() -> void:
-	pass # Replace with function body.
+func _on_credits_pressed() -> void:                 # When the credits button is pressed 
+	$Cover.show()                                   # Show the cover
+	$AnimationPlayer.play("cover")                  # Cover the start menu - "fade to black"
+	await get_tree().create_timer(1.0).timeout      # Wait 1 second to let the animation finish 
+	get_tree().change_scene_to_file("res://Credits scene/credits.tscn")   # Change the scence to the credits screen 
