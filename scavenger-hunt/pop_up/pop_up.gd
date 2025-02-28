76af3ef252ed9bc99b1864ed438e49e6538bdd4c
anysketch <glane0900@gmail.com>
2025-02-27 17:45:57 -0800

    Upload of scavenger hunt
    Issues with name change caused broken links and dependencies. New upload of whole minigame

---
+++ scavenger-hunt/pop_up/pop_up.gd
@@ -0,0 +1,14 @@
+extends TextureRect
+
+signal option1
+signal option2
+signal option3
+
+func _on_option_1_pressed() -> void:
+	option1.emit()
+
+func _on_option_2_pressed() -> void:
+	option2.emit()
+
+func _on_option_3_pressed() -> void:
+	option3.emit()
