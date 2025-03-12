99caf213f53d750456e5ab8fa666574ebdf73688
anysketch <glane0900@gmail.com>
2025-03-11 23:24:44 -0700

    Update globals.gd
    Dev skip able to be implemented in everyone's minigames

--- globals.gd
+++ globals.gd
@@ -1,23 +1,24 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var input_buffer = ""
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_pressed("1"):
+	if event is InputEventKey and event.pressed:
+		if event.unicode > 0:  # Check if it's a printable character
+			input_buffer += char(event.unicode)
+		
+	if event.is_action_pressed("ui_accept"):  # When Enter is pressed
+		process_input(input_buffer)
+		input_buffer = ""  # Clear buffer for next input
+
+func process_input(input_text):
+	if input_text == ("1"):
 		get_tree().change_scene_to_file("res://scavenger-hunt/world.tscn")
-	if Input.is_action_pressed("2"):
+	if input_text == ("2"):
 		get_tree().change_scene_to_file("res://meteoroid-level/scenes/level_1_screen.tscn")
-	if Input.is_action_pressed("3"):
+	if input_text == ("3"):
 		get_tree().change_scene_to_file("res://marsslingshot2.0/slingshot/world.tscn")
-	if Input.is_action_pressed("4"):
+	if input_text == ("4"):
 		get_tree().change_scene_to_file("res://psychescan/levels/main_level.tscn")
-	if Input.is_action_pressed("5"):
+	if input_text == ("5"):
 		get_tree().change_scene_to_file("res://Finished Screen/Scene.tscn")
