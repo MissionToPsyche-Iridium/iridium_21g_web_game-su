0f092fe9cf2d1bfc7a59b55ca65b8477093446ce
Darkbolt200 <lylev2003@gmail.com>
2025-04-21 21:12:00 -0700

    Add files via upload

--- Typing Level/typing_level.gd
+++ Typing Level/typing_level.gd
@@ -9,7 +9,21 @@ var startPos
 var lineTimer = 0
 var lineTime = 0
 var textBox = false
+var state = 0
+var nextLeter = 0
+var label
+var animationPlayer
+var playing = false
 const lineSpeed = 100
+
+
+var rightTextBlocks = [
+	"The Psyche sattellite will send out words. Hit [Enter] to continue",
+	" To complete this section, type the words to analyze what they are.",
+	"The sattellite will continue to send out information until all information has been recieved."
+]
+
+
 #[name, delay, text
 var textBlocks = [
 	["Psyche", 4.5, "A unique metal asteroid which provides a window into the formation of planetary cores."],
@@ -24,6 +38,10 @@ var textBlocks = [
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	startPos = $Node2D.position
+	label = $Node2D2/Label
+	animationPlayer = $Node2D2/AnimationPlayer
+	label.text = rightTextBlocks.pop_front()
+	label.visible_ratio = 0
 	return
 	texts = text.instantiate(0)
 	add_child(texts)
@@ -34,7 +52,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if state == 0:
+		cycleText(delta)
+		return
+	if state == 1:
+		if not playing:
+			animationPlayer.play("Start Cutscene")
+			playing = true
+			
+		return
 	if textBox:
+		
 		pass#simply exists as a check
 	else:
 		lineTimer += delta
@@ -70,6 +98,17 @@ func addBlock():
 	blocks.append(newText)
 
 func _unhandled_input(event):
+	if state == 0:
+		if event.is_action_pressed("ui_accept"):
+			if label.visible_ratio < 1:
+				return
+			if rightTextBlocks.size() > 0:
+				label.visible_ratio = 0
+				nextLeter = 0
+				label.text = rightTextBlocks.pop_front()
+			else:
+				state+=1
+		return
 	if textBox:
 		return
 	if event is InputEventKey and not event.is_pressed():
@@ -84,4 +123,14 @@ func _unhandled_input(event):
 func _on_button_button_up() -> void:
 	$Control.visible = false
 	textBox = false
+	pass # Replace with function body.
+
+func cycleText(delta):
+	if label.visible_ratio < 1:
+		nextLeter += delta*30
+		label.visible_characters = nextLeter
+		
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	state = 2
 	pass # Replace with function body.
