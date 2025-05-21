ea90051ce5ab2f4af9ff04058deaa28d6124c1b1
ckcurry <159097633+ckcurry@users.noreply.github.com>
2025-05-20 22:48:00 -0700

    Deleted global references in end_scene.gd

--- Godot Files/marsslingshot2.0/scripts/end_scene.gd
+++ Godot Files/marsslingshot2.0/scripts/end_scene.gd
@@ -3,15 +3,12 @@ extends Node2D
 var velocity1 = 0.00
 var velocity2 = 0.00
 
-func display_velocity():
-	$Velocity1Label.text = str(Globals.velocity1)
-	$Velocity2Label.text = str(Globals.velocity2)
 	
 func set_vel_label():
 	velocity1 = $space_ship.velocity.x
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	display_velocity()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
