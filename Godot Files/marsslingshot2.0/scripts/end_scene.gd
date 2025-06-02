ff6dea842f754d5e713f5b1f5753354a9a016a20
ckcurry <159097633+ckcurry@users.noreply.github.com>
2025-06-01 20:23:19 -0700

    uploading correct file to update end_scene

--- Godot Files/marsslingshot2.0/scripts/end_scene.gd
+++ Godot Files/marsslingshot2.0/scripts/end_scene.gd
@@ -1,14 +1,81 @@
-extends Node2D
+# script for the end screen dialogue
 
-# Called when the node enters the scene tree for the first time.
+extends Control
+
+var text = [
+	"Great work! The Psyche Mission is using
+	
+	the sling shot maneuver. This maneuver
+	
+	is used to set trajectory and speed
+	
+	using minimal fuel. The Psyche Mission
+	
+	uses mars as a gravitational assist. 
+	
+	This allows the spacecraft to speed
+	
+	up, slow down and change course."
+]
+@onready var message: Label = $Message
+@onready var next_level_button: Button = $NextLevelButton
+@onready var typing_sound: AudioStreamPlayer2D = $TypingSound
+
+const base_speed := 40
+var started := true
+var finished := false
+var count := 0.0
+var covered = true
+var start = false
+
 func _ready() -> void:
-	pass
+	next_level_button.hide() # hide button until dialogue done
+	await get_tree().create_timer(1.0).timeout
+	start = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# function to read stream of text and type it out
 func _process(delta: float) -> void:
-	pass
+	if (start):
+		if(message.get_visible_ratio() != 1.0):
+			if(count < 1):
+				count += base_speed*delta
+			else:
+				message.set_visible_characters(message.get_visible_characters()+1)
+				if (message.get_visible_characters() % 2 == 0):
+					typing_sound.play()
+				count -= 1
+		else:
+			if text.is_empty() && !finished:
+				finished = true
+			else:
+				if (covered):
+					covered = false
+		if(started):
+			changeText()
+			started = false
+		if (finished):
+			covered = false
+			next_level_button.show()
 
+func changeText():
+	if text.size() > 0:
+		message.set_visible_ratio(0.0)
+		message.text = text.pop_front()
+	else: 
+		finished = true
 
-func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://psychescan/levels/main_level.tscn")
+# can skip dialogue
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		covered = true
+		if(message.get_visible_ratio() == 1.0):
+			changeText()
+		else:
+			message.set_visible_ratio(1.0)
+	if event.is_action_pressed("interact"):
+		while text.size() != 0:
+			covered = true
+			if(message.get_visible_ratio() == 1.0):
+				changeText()
+			else:
+				message.set_visible_ratio(1.0)
